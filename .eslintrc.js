a9149e82a33e0e104033e75648b8b41d11c5968f
HUNTER\EMagallanes <elijah.magallanes@hunterindustries.com>
2019-01-09 07:42:16 -0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    root: true,
+    parserOptions: {
+        sourceType: "module"
+    },
+    extends: "standard",
+    plugins: [
+        "html"
+    ],
+    "rules": {
+        "comma-dangle": 0,
+        "indent": ["error", 4]
+    }
+};
