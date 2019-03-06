d57d60e43c98e7f88ff177dba76ee07800daa934
Eli Magallanes <elimaga1996@gmail.com>
2019-03-05 21:19:57 -0800

    Broke Layout down using Child Components

--- src/theme/Layout.vue
+++ src/theme/Layout.vue
@@ -1,56 +1,27 @@
 <template>
     <div>
-        <nav class="nav has-shadow">
-          <div class="container">
-            <a href="/">
-              <img src="http://bit.ly/vue-img" alt="Vue SPA" />
-            </a>
-          </div>
-        </nav>
+        <app-header></app-header>
         <section class="main-section section">
             <div class="container content">
-                <div class="columns">
-                    <div class="column is-one-third" v-for="(post, title) in posts" v-bind:key="post.id">
-                        <div class="card">
-                            <div class="card-content">
-                                <h3>{{ post.title }}</h3>
-                                {{ post.content }}
-                            </div>
-                            <footer class="card-footer">
-                                <a class="card-footer-item" :href="post.link" target="_blank">Read More</a>
-                            </footer>
-                        </div>
-                    </div>
-                </div>
+                <category></category>
             </div>
         </section>
-        <footer class="footer">
-          <div class="container">
-            <div class="content has-text-centered">
-              Follow us on
-              <a href="https://twitter.com/bstavroulakis"
-              target="_blank">Twitter</a>
-            </div>
-          </div>
-        </footer>
+        <app-footer></app-footer>
     </div>
 </template>
 
 <script>
-export default {
-    data () {
-        return {
-            posts: [
-                { id: 1, title: 'PWA Stats', content: 'A community-driven list of stats and news related to ...' },
-                { id: 2, title: 'A Comprehensive Guide to HTTP/2 Server Push', content: 'No longer is HTTP/2 ...' },
-                { id: 3, title: 'So whats this GraphQL thing I keep hearing about?', content: 'Why now is the ...' },
-                { id: 4, title: 'State of The Mobile Gap Between Native and Web', content: 'Clearly PhoneGap ...' },
-                { id: 5, title: 'Learning Javascript Design Patterns', content: 'Design patterns are reusable ...' },
-                { id: 6, title: 'The Power of Custom Directives in Vue', content: 'The beautiful thing about ...' }
-            ]
+    import AppHeader from './AppHeader.vue'
+    import AppFooter from './AppFooter.vue'
+    import Category from './Category.vue'
+
+    export default {
+        components: {
+            'app-header': AppHeader,
+            'app-footer': AppFooter,
+            'category': Category
         }
     }
-}
 </script>
 
 <style lang="scss">
