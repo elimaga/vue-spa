d5b8c04d04b57901511d5656d87878386460005b
HUNTER\EMagallanes <elijah.magallanes@hunterindustries.com>
2019-03-06 07:42:43 -0800

    Added tabs to load the home page and the login page;
    Using the router-link component prevents entire page from reloading

--- src/theme/Category.vue
+++ src/theme/Category.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="columns">
-        <div class="column is-one-third" v-for="(post, title) in posts" v-bind:key="post.id">
+        <div class="column is-one-third" v-for="post in posts" v-bind:key="post.id">
             <app-post :link="post.link">
                 <h3 slot="title">{{ post.title }}</h3>
                 <span slot="content">{{ post.content }}</span>
